7dc8e9b36fdd36d718a034091067f7361dbd59b9
Mohamed <mohamed.alkaabi@hotmail.com>
2023-03-05 15:05:01 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_RUI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Puppy(db.model):
+
+    __tablename__ = 'puppies'
+
+    id = db.Column(db.integer, primary_key=True)
+    name = db.Column(db.Text)
+    age = db.Column(db.Integer)
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age  = age
+
+    def __repr__(self):
+        return 'name: {}, age : {}'.format(self.name,self.age)
